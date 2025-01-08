e496588cc5a1e8b0a308da84aaa8299831492c88
Chris Phillips <cminion@gmail.com>
2025-01-08 22:24:12 +0000

    remove cache headers

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const port = 3000
 const payload = require('./256KB.json')
 
 
-const nocache = require('nocache');
-
-
 
 app.get('/', (req, res) => {
   res.set('Cache-Control', 'max-age=300, must-revalidate')
